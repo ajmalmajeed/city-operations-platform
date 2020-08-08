b5ce91e444ba2f5c96072c03018614bf3dee0d22
Ajmal Abdul Majeed <ajmalabdulmajeed@192.168.1.3>
2020-08-08 17:32:49 +0530

    added firebase settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AngularFireModule} from '@angular/fire';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {environment} from '../environments/environment';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebaseConfig: {
+  production: false,
+  apiKey: 'AIzaSyAHSXhk4wCLCbvfjU_JhN_7F521IpNmp6M',
+  authDomain: 'mckinseyops.firebaseapp.com',
+  databaseURL: 'https://mckinseyops.firebaseio.com',
+  projectId: 'mckinseyops',
+  storageBucket: 'mckinseyops.appspot.com',
+  messagingSenderId: '430786175454',
+  appId: '1:430786175454:web:84a5fe00d384c60de67d4f',
+  measurementId: 'G-W72M30P4RX'
+  }
 };
 
 /*
